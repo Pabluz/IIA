4f19d987595379dd3e86a94b238a0c75cb3778e5
Ricardo Cruz <33420050+Pabluz@users.noreply.github.com>
2017-12-16 12:23:59 +0000

    Add files via upload

--- hobbes_jogo_NN.py
+++ hobbes_jogo_NN.py
@@ -4,28 +4,28 @@ def _init_():
 
 
 #retorna jogadas possiveis de um dado estado
-def actions():
+def actions(state):
     return null
 
 
 #retorna estado que se obtem a fazer uma jogada
-def result():
+def result(move):
     return null
 
 
 #Cálculo da utilidade de um estado na perspectiva de um dado jogador.
 #Deverá ter o valor 1, para o caso de vitória, ou -1 em caso de derrota.
-def utility():
+def utility(state,jogador):
     return null
 
 
 #metodo booleano que verific se um dado estado é final
-def terminal_test():
+def terminal_test(state):
     return null
 
 
 #Mostra uma representação de um estado do jogo
-def display():
+def display(state):
     return null
     
 
