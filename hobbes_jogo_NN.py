04093f89b91e63bc00f76ab039c86344ef78946a
Ricardo Cruz <33420050+Pabluz@users.noreply.github.com>
2017-12-19 17:06:05 +0000

    Add files via upload

--- hobbes_jogo_NN.py
+++ hobbes_jogo_NN.py
@@ -12,13 +12,23 @@ class JogoHobbes(jogos_iia.Game) :
     jogadores = ('rei_preto,rei_branco')
 
 
+    board = {(2, 1): 'n', (2, 2): 'n', (2, 4): 'n', (2, 5): 'n',
+    (3, 1): 'p', (3, 2): 'n', (3, 4): 'n', (3, 5): 'b',
+    (4, 1): 'n', (4, 2): 'n', (4, 4): 'n', (4, 5): 'n'}
+
+    moves = [((3,1),(2,1)),((3,1),(3,2)),((3,1),(4,1))]
+    initial = jogos_iia.GameState(jogadores[0],0,(0,board),moves)
+    #construtor do jogo hobbes
+    def _init_(self):
+        tabuleiro = board
+        movimentos = moves
+        initial = initial
+
+    
     @staticmethod
     def outro_jogador(j) :
         return 'rei_preto' if j == 'rei_branco' else 'rei_preto'
 
-    #construtor do jogo hobbes
-    def _init_(self):
-        return
 
     @staticmethod
     def conv_peca(j):
