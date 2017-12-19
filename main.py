fcfe92bd72dd28f8b6ee3f506bfce6c63ec6dd6b
Ricardo Cruz <33420050+Pabluz@users.noreply.github.com>
2017-12-19 17:06:22 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-# -*- coding: utf-8 -*-
-## Main - Jogo dos Peões
-
-#Módulos genéricos
-import jogar     # utlidades para realizar jogos
-
-# Módulos específicos do jogo dos peões
+import jogar
 import hobbes_jogo_NN
-import jogos_iia
-from copy import deepcopy
 
+jogo_hobbes = hobbes_jogo_NN.JogoHobbes()
 
-board = {(2, 1): 'n', (2, 2): 'n', (2, 4): 'n', (2, 5): 'n',
-(3, 1): 'p', (3, 2): 'n', (3, 4): 'n', (3, 5): 'b',
-(4, 1): 'n', (4, 2): 'n', (4, 4): 'n', (4, 5): 'n'}
+j1 = jogar.Jogador(jogo_hobbes,"Ze",f = jogar.random_player)
+j2 = jogar.Jogador(jogo_hobbes,"Ao Calhas",f = jogar.random_player)
 
-moves = [((3,1),(2,1)),((3,1),(3,2)),((3,1),(4,1))]
+result = jogar.um_jogo(jogo_hobbes,j1,j2,0,True)
 
-state = jogos_iia.GameState('rei_preto',0,(0,board),moves)
-
-jogo_hobbes =hobbes_jogo_NN.JogoHobbes()
-
-jogo_hobbes.display(state)
-
-acoes = jogo_hobbes.actions(state)
-
-
-
-for x in acoes:
-    orignal_state = deepcopy(state)
-    jogo_hobbes.display(jogo_hobbes.result(state,x))
-    state = orignal_state
-    jogo_hobbes.display(state)
-
+print(result)
